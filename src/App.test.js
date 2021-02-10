52f4c4ea558299be50b5ac265b7c84b272665d9d
cxp723 <kirichenko723@gmail.com>
2021-02-10 23:07:02 +0300

    Few tests for films reducer actions added

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import SocialNetwork from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import SocialNetwork from "./App";
 
-test('renders learn react link', () => {
+test("renders preloader while app initializing", () => {
   const { getByTestId } = render(<SocialNetwork />);
-  const preloader = getByTestId('preloader');
+  const preloader = getByTestId("preloader");
   expect(preloader).toBeInTheDocument();
 });
